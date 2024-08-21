3c07d3a665355b2905933e519bdae30a17003f27
Arlingga Wirawan Jayasaputra <arlingga.wirawan@gpi.id>
2024-08-21 17:16:20 +0700

    add headless function in hooks, headless default: false

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -1,18 +1,31 @@
 const { Before, After } = require('@cucumber/cucumber');
-const { Builder, Capabilities } = require('selenium-webdriver');
+const { Builder } = require('selenium-webdriver');
+const config = require('../../config/testConfig');
+const chrome = require('selenium-webdriver/chrome');
 require('chromedriver');
 
 let driver;
 
-const capabilities = Capabilities.chrome();
-capabilities.set('chromeOptions', { "w3c": false });
-
 Before(async function () {
-    driver = new Builder().withCapabilities(capabilities).build();
+    let option = new chrome.Options();
+    
+    if (config.headless) {
+        option.addArguments('--headless');
+        option.addArguments('--window-size=1920,1080');
+    }
+
+    driver = await new Builder()
+        .forBrowser(config.browser)
+        .setChromeOptions(option)
+        .build();
+
     this.driver = driver;
     console.log('WebDriver initialize');
-    await this.driver.manage().window().maximize();
-    console.log('Browser window maximized.');
+
+    if (!config.headless) {
+        await this.driver.manage().window().maximize();
+        console.log('Browser window maximized.');
+    }
 });
 
 After(async function () {
